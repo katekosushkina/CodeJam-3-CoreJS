30ae0b2b395f179ded001afe1e14e5ec4ffd3376
katekosushkina <kate.kosushkina@gmail.com>
2018-10-29 12:36:37 +0300

    hotfix: move sum function to make.spec.js

--- make.js
+++ make.js
@@ -1,17 +1,14 @@
-module.exports = (function make(n) {
-    const values = [n];
-    function makeInternal(...args) {
-      for (let i = 0; i < args.length; i += 1) {
-        const arg = args[i];
-        if (typeof arg === 'function') {
-          return values.reduce(arg);
-        }
-        values.push(arg);
+module.exports = function make(n) {
+  const values = [n];
+  function makeInternal(...args) {
+    for (let i = 0; i < args.length; i += 1) {
+      const arg = args[i];
+      if (typeof arg === 'function') {
+        return values.reduce(arg);
       }
-      return makeInternal;
+      values.push(arg);
     }
     return makeInternal;
-  }());
-  function sum(a, b) {
-      return a + b;
-    }
+  }
+  return makeInternal;
+};
--- make.spec.js
+++ make.spec.js
@@ -1,11 +1,15 @@
 const expect = require('chai').expect;
 const make = require('./make.js');
 
+const sum = function sum(a, b) {
+  return a + b;
+};
+
 describe('make', () => {
   it('function call 4 times', () => {
     expect(777).to.eql(make(15)(34, 21, 666)(41)(sum));
   });
-  it('make', () => {
-    expect(1596).to.eql(console.log(make(15)(34, 21, 666)(41)(42)(sum)));
+  it('function call 5 times', () => {
+    expect(819).to.eql(make(15)(34, 21, 666)(41)(42)(sum));
   });
 });
