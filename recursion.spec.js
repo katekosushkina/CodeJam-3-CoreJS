3969dccd37ec1fc2548b2e57b1122239932e115a
katekosushkina <kate.kosushkina@gmail.com>
2018-10-29 12:40:30 +0300

    fix: fix test for recursion function

--- recursion.spec.js
+++ recursion.spec.js
@@ -3,7 +3,7 @@ const recursion = require('./recursion.js');
 
 describe('recursion', () => {
   it('recursion1', () => {
-    expect([[100],[90,120],[70,99,110,130]]).to.eql(JSON.stringify(recursion({
+    expect([[100], [90, 120], [70, 99, 110, 130]]).to.eql(recursion({
       value: 100,
       left: {
         value: 90,
@@ -11,6 +11,6 @@ describe('recursion', () => {
         right: { value: 99 },
       },
       right: { value: 120, left: { value: 110 }, right: { value: 130 } },
-    })));
+    }));
   });
 });
